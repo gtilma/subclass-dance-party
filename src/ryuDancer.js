3bab5129af76715b47d75c7ad004b1a4ef562bb6
MKS-Student <mks.student@hackreactor.com>
2015-03-10 19:28:36 -0500

    implements intersection of dancers

--- src/ryuDancer.js
+++ src/ryuDancer.js
@@ -18,6 +18,26 @@ RyuDancer.prototype.constructor = RyuDancer;
 RyuDancer.prototype.step = function(){
   Dancer.prototype.step.call(this);
   Dancer.prototype.setPosition.call(this, $("body").height() * Math.random(), $("body").width() * Math.random())
+  
+  var x1 = this.$node.css('left');
+  x1 = +x1.substring(0, x1.length - 2);
+  var y1 = this.$node.css('top');
+  y1 = +y1.substring(0, y1.length - 2)
+  var y2, x2, between;
+  
+  for (var i = 0; i < window.dancers.length; i++) {
+    if (window.dancers[i] instanceof RyuDancer){
+      continue;
+    }
+    x2 = window.dancers[i].$node.css('left')
+    x2 = +x2.substring(0, x2.length - 2)
+    y2 = window.dancers[i].$node.css('top')
+    y2 = +y2.substring(0, y2.length - 2)
+    between = Math.sqrt(Math.pow(Math.abs(x1 - x2), 2) + Math.pow(Math.abs(y1 - y2), 2))
+    if (between < 200){
+      window.dancers[i].$node.css('height', '200px');
+    }
+  }
 };
 
 RyuDancer.prototype.spin = function(){
