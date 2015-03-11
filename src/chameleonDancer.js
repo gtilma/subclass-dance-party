99b2ef01e74658d332039f0f2f37206980caffeb
MKS-Student <mks.student@hackreactor.com>
2015-03-10 20:08:32 -0500

    Fixes semicolons & cleans up

--- src/chameleonDancer.js
+++ src/chameleonDancer.js
@@ -1,5 +1,5 @@
 var ChameleonDancer = function(top, left, timeBetweenSteps){
-  Dancer.call(this, top, left, timeBetweenSteps);
+    Dancer.call(this, top, left, timeBetweenSteps);
 };
 
 ChameleonDancer.prototype = Object.create(Dancer.prototype);
@@ -9,6 +9,7 @@ ChameleonDancer.prototype.step = function(){
   Dancer.prototype.step.call(this);
   this.$node.css('border-color', this.getColor());
 };
+
 ChameleonDancer.prototype.getColor = function(){
   var hexArray = [1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
   var color = '#';
